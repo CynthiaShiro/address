487bade426a3badf88ae9a8ffe448cc1e277863d
CynthiaShiro <aimydewlux@gmail.com>
2020-09-15 18:43:51 +0300

    initial commit

--- file.py
+++ file.py
@@ -1,9 +1,30 @@
-f = open("C:/Users/user/Desktop/Cynthia/student.txt.txt", "r")
-print(f.read())
-f.close()
+import camelcase 
+import re 
+def getrate(amount):
+    if (amount == 1000):
+        print("The amount is equal to 1000!")
+    elif (amount <= 1000):
+        return amount * 0.98
+    else:
+        print("Transaction complete!!")
+def validate_pin(pin):
+    for x in pin:
+        if x.isalpha():
+            return False
+        elif re.match(".", pin):
+            return False
+    if len(pin) == 4 or len(pin) == 6:
+        return True
+    else:
+        return False
+        
+PIN = input("Enter a PIN: ")
+validate_pin(PIN) 
+c = camelcase.CamelCase()
+txt = "start the transaction!!!"
+print(c.hump(txt))
 
-f1 = open("C:/Users/user/Desktop/Cynthia/student.txt.txt", "w")
-f1.write("This is an extra text added on the app!")
-f1 = open("C:/Users/user/Desktop/Cynthia/student.txt.txt", "r")
+Amount = int(input("Enter the Amount:"))
 
-print(f1.read())
+print(getrate(Amount))
+
